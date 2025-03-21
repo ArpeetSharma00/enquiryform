19fdc3135fa45eab715f1af3e8ace24f44c83918
Arpeet Sharma <arpeet047@gmail.com>
2025-03-21 11:45:07 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "your_host";  // Example: "sqlXXX.epizy.com"
-$username = "your_username";  // Example: "epiz_12345678"
-$password = "your_password";
-$database = "your_database_name";  // Example: "epiz_12345678_dbname"
+$host = "sql311.infinityfree.com";  // Example: "sqlXXX.epizy.com"
+$username = "if0_38567974";  // Example: "epiz_12345678"
+$password = "Reversalred1234";
+$database = "if0_38567974_enquirydatabase";  // Example: "epiz_12345678_dbname"
 
 $conn = new mysqli($host, $username, $password, $database);
 
